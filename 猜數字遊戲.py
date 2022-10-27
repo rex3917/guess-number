8129932998d97ac0f70242f61e33a65c5a3a066c
Louis Lin <louis3917@gmail.com>
2022-10-27 17:43:44 +0800

    second commit

--- 猜數字遊戲.py
+++ 猜數字遊戲.py
@@ -1,10 +1,13 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
     ans = int(input('請在1~100內猜數字: '))
+    count += 1
     if ans == r:
         print('終於猜對了!')
+        print('總共猜了', count, '次')
         break
     elif r > ans:
         print('比答案小')

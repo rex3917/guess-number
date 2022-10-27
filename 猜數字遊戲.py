3a165c77253d9551bcb803a6dad2238bada0ab6c
Louis Lin <louis3917@gmail.com>
2022-10-27 17:50:43 +0800

    Update commit

--- 猜數字遊戲.py
+++ 猜數字遊戲.py
@@ -1,9 +1,13 @@
 import random
 
-r = random.randint(1, 100)
+a = int(input('請決定隨機數字範圍開始值: '))
+b = int(input('請決定隨機數字範圍結束值: '))
+r = random.randint(a, b)
 count = 0
 while True:
     ans = int(input('請在1~100內猜數字: '))
+    if ans > b or ans < a:
+        print('數字超出範圍')
     count += 1
     if ans == r:
         print('終於猜對了!')
